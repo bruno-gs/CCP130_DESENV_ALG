a445e1c09d8091b6fb14311a736f5d757eb5e18a
bruno-gs <bgottsfritz@gmail.com>
2021-08-24 19:47:18 -0300

    ATV2 Finalizada

--- AULA 3 - 20AGO/LAB/ex5.c
+++ AULA 3 - 20AGO/LAB/ex5.c
@@ -18,3 +18,31 @@ Saída: Os valores devem ser mostrados na mesma linha,
 separados por um espaço em branco. 
 Não deve haver espaço após o último valor.
 */
+
+#include <stdio.h>
+
+int main(void){
+    int t2, t0=0, t1=1, termos, cont = 3;
+
+    // pedindo a quantidade de termos da sequencia
+    printf("Digite um número: \n");
+    scanf(" %d",&termos);
+
+    printf("%d %d ", t0, t1);
+
+    while(cont<=termos){
+
+        // novo numero como soma dos dois ultimos
+        t2 = t1 + t0;
+        printf("%d ", t2);
+
+        // troca de lugar entre os valores
+        // O t0 é esquecido, t1 vira t0 e t2 (novo) vira t1
+        t0 = t1;
+        t1 = t2;
+
+        // incrementa cont para continuar passando os valores
+        cont ++;
+    }
+    return 0;
+}
--- AULA 3 - 20AGO/LAB/ex6.c
+++ AULA 3 - 20AGO/LAB/ex6.c
@@ -11,3 +11,32 @@ verifique se o número fornecido é primo ou não.
 Mostre uma mensagem de número primo ou de número não primo. 
     Um número é primo quando é divisível apenas pelo número um e por ele mesmo.
 */
+
+#include <stdio.h>
+
+int main(void){
+    int num, total=0;
+
+    // Qual numero deseja saber que é primo
+    printf("Digite um número inteiro: \n");
+    scanf(" %d", &num);
+
+    // fará a conferencia em cada valor até o desejado
+    // somará ao total quando for divisivel
+    for(int seq = 1; seq <= num; seq++){
+        if(num % seq == 0){
+            total ++;
+        }
+    }
+
+    // total == 2 pois tem o 1 e o valor
+    if(total==2){
+        printf("O número %d é primo.", num);
+    }
+
+    // senão não é primo
+    else{
+        printf("O número %d não é primo.", num);
+    }
+    return 0;
+}

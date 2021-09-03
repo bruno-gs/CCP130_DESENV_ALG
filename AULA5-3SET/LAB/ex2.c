36b9da38c994f57b7afcf2797d8c3a8efe8205bd
bruno-gs <bgottsfritz@gmail.com>
2021-09-03 17:11:06 -0300

    aula 5 - final - Inicio do lab

---
+++ AULA5-3SET/LAB/ex2.c
@@ -0,0 +1,8 @@
+/*
+Autor : FRITZ
+DATA: 02/09/2021
+
+Crie uma matriz m[8][8] com números inteiros aleatórios 
+    (de 0 até 100, rand() % 101;). 
+em seguida realize a soma dos elementos da diagonal principal.
+*/
---
+++ AULA5-3SET/LAB/ex3.c
@@ -0,0 +1,9 @@
+/*
+Autor : FRITZ
+DATA: 02/09/2021
+
+Crie uma matriz m[12][12] com números inteiros aleatórios (de 0 até 100, rand() % 101;) . 
+Em seguida, calcule e mostre a soma considerando somente aqueles elementos que 
+    estão abaixo da diagonal principal da matriz, 
+conforme ilustrado abaixo (área verde ).
+*/

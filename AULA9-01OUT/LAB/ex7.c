8c2409a47ca8d810acd6ed350776cf0c5c7ec691
BrunoGSilva <52896573+bruno-gs@users.noreply.github.com>
2021-10-14 18:04:39 -0300

    'Atv 8 - Finalizada'

--- AULA9-01OUT/LAB/ex7.c
+++ AULA9-01OUT/LAB/ex7.c
@@ -1,6 +1,6 @@
 /*
 Autor : FRITZ
-DATA: 05/09/2021
+DATA: 14/10/2021
 
 Escreva um programa baseado no Exercício 4 que recebe uma frase 
     e determina as ocorrências totais de cada letra do alfabeto. 
@@ -17,8 +17,34 @@ Armazene os totais para cada letra em um array e
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+int main(void){
 
-    
-    return 0;
+  char s[50]={"asd"};
+  char Mi= 97,Ma=65;
+  int v[26], x=0;
+
+  for(int f=0; f<26;f++){
+    v[f]=0;
+  }
+
+  puts("Digite uma frase:");
+  fgets(s,50,stdin);
+  
+  
+  for (int x=0; x < 26; x++){
+    for (int i=0; i < 50; i++){
+      if(s[i] == Ma || s[i] == Mi){
+        v[x]++;
+      }
+    }
+    Ma++;
+    Mi++;
+  }
+  Mi=97;
+  for(int p=0; p < 26; p++){
+    printf("O caractere %c foi encontrado %d vezes\n",Mi,v[p]);
+    Mi++;
+  }
+
+  return 0;
 }
